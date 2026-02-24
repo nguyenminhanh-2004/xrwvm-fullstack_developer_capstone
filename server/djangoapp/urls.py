0fa20cca200ff91f87d5544cfa78438395e690f7
nguyenminhanh-2004 <kenshibroly1@gmail.com>
2026-02-24 14:45:12 +0700

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,12 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
-# from . import views
+from django.urls import path
+from . import views
 
-app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
-
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('login', views.login_user, name='login'),
+    path('logout', views.logout_request, name='logout'),
+    path('fetchDealers', views.get_dealerships, name='get_dealers'),
+    path('fetchDealers/<str:state>', views.get_dealerships, name='get_dealers_by_state'),
+    path('fetchDealer/<int:dealer_id>', views.get_dealer_details, name='get_dealer_details'),
+    path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='get_dealer_reviews'),
+    path('analyze/<str:text>', views.analyze_review_sentiments, name='analyze_sentiment'),
+]
